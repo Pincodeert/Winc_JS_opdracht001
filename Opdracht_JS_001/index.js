de66ed7f27c39bcc94e09d649e9adf7f0fdc45a2
Pincodeert <84202600+Pincodeert@users.noreply.github.com>
2021-07-05 14:39:08 +0200

    Add files via upload

---
+++ Opdracht_JS_001/index.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy")
+          let naam = 'Leonie';
+          console.log(naam)
+          console.log(4+4) 
+          console.log("4"+"4" )         
+          console.log(8+8)
+          console.log(8-12) 
+          console.log(8*8)
+          console.log(12/5)
+          console.log(20%80)
+          let leeftijd = 47
+          console.log(leeftijd)
+          let age = 'zevenveertig'
+          console.log(age)
